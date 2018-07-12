9a6e9edf861c766b2799250193653ea3f61e452a
Marvin Do <marvin.do@everykey.com>
2018-07-12 11:20:09 -0700

    Added Delegates for Search Bar and Table View

---
+++ StoreSearchProxy/SearchResult.swift
@@ -0,0 +1,14 @@
+//
+//  SearchResult.swift
+//  StoreSearchProxy
+//
+//  Created by Marvin Do on 7/12/18.
+//  Copyright © 2018 Marvin Do. All rights reserved.
+//
+
+import Foundation
+
+class SearchResult {
+    var name = ""
+    var artistName = ""
+}
