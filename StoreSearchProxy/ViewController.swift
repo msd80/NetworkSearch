c276a059a38c6273148b01bcee780ef5c71d96f6
Marvin Do <marvin.do@everykey.com>
2018-07-12 09:49:26 -0700

    Initial Commit

---
+++ StoreSearchProxy/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StoreSearchProxy
+//
+//  Created by Marvin Do on 7/12/18.
+//  Copyright © 2018 Marvin Do. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
